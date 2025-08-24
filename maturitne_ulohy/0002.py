759f5b4df56448e151d0e8b1943e6db89052bb96
jsz <ju.szarka@gmail.com>
2025-08-24 20:14:29 +0200

    maturitna_uloha_2 v1.0 (final)

--- maturitne_ulohy/0002.py
+++ maturitne_ulohy/0002.py
@@ -1,17 +1,44 @@
-f = open("hada.txt", "r")
+import os
+
+f = open("content/hada.txt", "r")
 
 content = f.read()
 line_list = content.split('\n')
 
-print(f)
-print('')
-print(content)
-print('')
-print(line_list)
-
-print('')
-
 def get_game_num(game_list = line_list):
     return len(game_list)
 
+def get_longest_game(game_list = line_list):
+    longest = ''
+    for each in game_list:
+        if len(each) > len(longest):
+            longest = each
+    return longest
+
+def create_copy(game_list = line_list):
+    result = ''
+    comprimed = ''
+    last = ''
+    num = 0
+    for each in game_list:
+        for i in each:
+            if num == 0:
+                last = i
+                num += 1
+            elif i == last:
+                num += 1
+            else:
+                comprimed = comprimed + last + str(num)
+                last = i
+                num = 1
+        comprimed = comprimed + last + str(num)
+        result += (comprimed + '\n')
+        comprimed = ''
+    with open("content/hada_copy_comprimed.txt", "w") as f2:
+        f2.write(result)
+
+    return result
+
 print(get_game_num())
+print(get_longest_game())
+print(create_copy())
