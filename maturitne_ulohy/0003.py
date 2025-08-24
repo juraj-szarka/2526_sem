669f1d357b96ff516b3a3d56b5ec58817ddbc111
jsz <ju.szarka@gmail.com>
2025-08-25 00:30:57 +0200

    bod 1 ulohy 3 hotový

--- maturitne_ulohy/0003.py
+++ maturitne_ulohy/0003.py
@@ -4,9 +4,21 @@ f = open('content/zastavky.txt', 'r', encoding='utf-8')
 
 zastavky = f.read()
 
+def load_stations(stations = zastavky):
+    stations_1 = stations.split('\n')
+    for i in range(len(stations_1) - 1):
+        if len(stations_1[i]) < 16:
+            stations_1[i] += (20 - len(stations_1[i])) * ' '
+        else:
+            stations_1[i] += 4 * ' '
+    stations_1[-1] += '    konečná zástavka, prosíme nenastupovať    '
+    print(stations_1)
+
+stations_displayable = []
+
 canvas = tk.Canvas()
 canvas.pack()
 
-print(zastavky)
+load_stations()
 
 canvas.mainloop()
