2246d44d6c9470755c15028877deb7cd529ea620
jsz <ju.szarka@gmail.com>
2025-08-25 23:51:56 +0200

    uloha 3 hotova (vela bugov stale)

--- maturitne_ulohy/0003.py
+++ maturitne_ulohy/0003.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+import time
 
 f = open('content/zastavky.txt', 'r', encoding='utf-8')
 
@@ -6,6 +7,8 @@ zastavky = f.read()
 station_index = 0
 stations = []
 current = ''
+state = 0
+instances = []
 
 def load_stations(stations = zastavky):
     stations_1 = stations.split('\n')
@@ -24,7 +27,7 @@ def load_instance(text: str):
     for i in range(l):
         to_append = ''
         for j in range(20):
-            to_append += staged_text[j]
+            to_append += staged_text[j+i]
         loaded.append(to_append)
 
     return loaded
@@ -35,6 +38,20 @@ def rewrite_text():
     text = canvas.create_text(200, 25, font=('Courier', 24), text=current, fill='red')
 
 
+
+def roll():
+    global current
+    global state
+    global instances
+    current = instances[state]
+    print(instances)
+    print(instances[state])
+    rewrite_text()
+    if (state + 1) == len(instances):
+        state = 0
+    else:
+        state += 1
+    canvas.after(250, roll)
 
 def start():
     global stations
@@ -49,16 +66,18 @@ def start():
 def on_click(event):
     global station_index
     global current
+    global stations
+    global state
+    global instances
+    state = 0
 
     if station_index == (len(stations) - 1):
         station_index = 0
     else:
         station_index += 1
-    current = stations[station_index]
-    rewrite_text()
-    print('clicked')
-    print(station_index)
-    print(current)
+    instances = load_instance(stations[station_index])
+    print(instances)
+    roll()
     return None
 
 canvas = tk.Canvas(width=400, height=50, background='black')
