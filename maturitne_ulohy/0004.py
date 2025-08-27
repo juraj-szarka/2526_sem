d081be5718617cb790cc9962539977cebf1d6dfe
jsz <ju.szarka@gmail.com>
2025-08-27 17:59:28 +0200

    mu4 bod4 + data manipulation improved

--- maturitne_ulohy/0004.py
+++ maturitne_ulohy/0004.py
@@ -9,7 +9,8 @@ def data_handling(data_txt):
     for each in data_list:
         each_splitted = each.split(" ")
         instance = {"station": each_splitted[0], "date": each_splitted[1], "time": each_splitted[2],
-                    "temp": each_splitted[3], "cloudiness": each_splitted[4]}
+                    "temp": float(each_splitted[3].replace(",", ".").replace("--", "-").replace("+-", "")),
+                    "cloudiness": each_splitted[4]}
         data_sorted.append(instance)
     return data_sorted
 
@@ -25,7 +26,7 @@ def pocet_merani(data_given):
 def namerane_teploty(data_given):
     temps = []
     for each in data_given:
-        temps.append(float(each["temp"].replace(",", ".").replace("--", "-").replace("+-", "")))
+        temps.append(each["temp"])
     return temps
 
 def najvyssia_teplota(data_given):
@@ -33,8 +34,15 @@ def najvyssia_teplota(data_given):
     temps.sort()
     return temps[-1]
 
+def kod_stanica_max(data_given):
+    max = najvyssia_teplota(data_given)
+    for each in data_given:
+        if each["temp"] == max:
+            return each["station"]
+
 data = data_handling(data_txt)
 
 print(pocet_merani(data))
 print(namerane_teploty(data))
 print(najvyssia_teplota(data))
+print(kod_stanica_max(data))
