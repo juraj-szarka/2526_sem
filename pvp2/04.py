db7d196f8a84d4713836ec42b8ead47e966cd33e
jsz <ju.szarka@gmail.com>
2025-09-16 13:50:19 +0200

    nazvy upravene, 3604 hotova

--- pvp2/04.py
+++
@@ -1,7 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title('Customer feedback')
-root.geometry('300x200')
-
-label = tk.Label(root)
---
+++ pvp2/3604.py
@@ -0,0 +1,30 @@
+import tkinter as tk
+
+def positive():
+    f = open('content/reviews.txt', 'a')
+    f.write('Áno\n')
+    f.close()
+
+def negative():
+    f = open('content/reviews.txt', 'a')
+    f.write('Nie\n')
+    f.close()
+
+root = tk.Tk()
+root.title('Customer feedback')
+root.geometry('300x200')
+
+
+label = tk.Label(root, text='Boli ste spokojný s našími službami?', font=('Arial', 12, 'bold'))
+label.pack(pady=30)
+
+frame = tk.Frame(root)
+frame.pack(expand=True)
+
+button = tk.Button(frame, text = "Áno", command=positive, background='green')
+button.pack(padx=10, side=tk.LEFT)
+
+button = tk.Button(frame, text = "Nie", command=negative, background='red')
+button.pack(padx=10, side=tk.LEFT)
+
+root.mainloop()
